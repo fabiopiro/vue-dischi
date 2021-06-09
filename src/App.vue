cccb9a4b7b377ae072849124a2544475f4c479ee
fabiopiro <piroddifabio01@gmail.com>
2021-06-09 17:19:40 +0200

    chiamata funzionante

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Nav />
+    <AlbumsContainer />
   </div>
 </template>
 
 <script>
-import Nav from './components/Nav.vue'
+import Nav from './components/Nav.vue';
+import AlbumsContainer from './components/AlbumsContainer.vue'
 
 export default {
   name: 'App',
   components: {
     Nav,
+    AlbumsContainer,
   }
 }
 </script>
