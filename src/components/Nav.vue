04cbb7babfcb1aeb931f07a7caa11ad6cec0566d
fabiopiro <piroddifabio01@gmail.com>
2021-06-09 16:07:12 +0200

    axios, bootstrap, fontsource e nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+      <img src="../assets/img/spotify.png" alt="logo">
+  </nav>
+</template>
+
+<script>
+export default {
+    name: 'Nav',
+}
+</script>
+
+<style lang='scss' scoped>
+    nav {
+        display: flex;
+        align-items: center;
+
+        height: 75px;
+        padding: 0 20px;
+        background-color: black;
+
+        img {
+            height: 50px;
+            width: 50px;
+        }
+    }
+</style>
